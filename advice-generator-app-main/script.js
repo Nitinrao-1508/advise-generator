284ab5e147f7d2180cd2216123637711bcba451a
aditya <adityarao21602@gmail.com>
2023-08-08 18:24:52 +0530

    second

--- advice-generator-app-main/script.js
+++ advice-generator-app-main/script.js
@@ -1,16 +1,25 @@
 const advicecall = document.querySelector('#generator');
-const advicetext = document.querySelector('#quotes');
-const advicebtn = document.querySelector('#btn');
+const advicetext = document.querySelector('.advice'); 
+const advicebtn = document.querySelector('.btn');
 
-fetch('https://api.adviceslip.com/advice')
-.then(response =>{
-    return response.json();
-}).then(newadvices =>{
-    const newadvices1 = advicedata.slip.id;
-    const newadvices2 = advicedata.slip.advice;
+function getadvice() {
+    fetch('https://api.adviceslip.com/advice')
+    .then(response => {
+        return response.json();
+    })
+    .then(newadvices => {
+        const newadvices1 = newadvices.slip.id;
+        const newadvices2 = newadvices.slip.advice;
 
+        advicecall.textContent = `advice#${newadvices1}`;
+        advicetext.innerHTML = `<q>${newadvices2}</q>`; // Wrapped advice in <q> tag
 
-    advicecall.textContent = newadvices1;
-    advicetext.
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
 
-})
+advicebtn.addEventListener('click', function(){ // Corrected typo in addEventListener
+    getadvice();
+});
